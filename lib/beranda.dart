a16f9240c3769e731aed9a6fcd80e49799917061
slametpolkam <slamet@poltek-kampar.ac.id>
2020-10-22 15:01:30 +0700

    Login dengan String TIF 1 A 22/10/2020

---
+++ lib/beranda.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halaman Kedua"),
+      ),
+      body: Center(
+        child: Text("Hallo selamat datang"),
+      ),
+    );
+  }
+}
